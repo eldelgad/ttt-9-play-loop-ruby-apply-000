c4916fd5b5bf9b1c3ec82f68c43558d37416c05b
Elias Delgado <evdelgado99@gmail.com>
2020-08-18 14:19:16 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,16 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play(board)
+  
+  game_turn = 0
+  
+  while game_turn < 9
+    
+    game_turn += 1
+    
+    turn(board)
+    
+  end
+end
