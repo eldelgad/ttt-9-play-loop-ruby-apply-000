eccff39bd7a2f6e074dbae5e94d14d1006b9fc6b
Elias Delgado <evdelgado99@gmail.com>
2020-08-18 14:15:32 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,13 +38,14 @@ end
 # Define your play method below
 
 def play(board)
-
+  
   turn = 0
-
-    while turn < 9
+  
+  while turn < 9
     
-      turn += 1
-      turn(board)
+    turn += 1
     
-    end
+    turn(board)
+    
+  end
 end
